6168c575c88e3b34f3a159f77a24d803dc18efc5
Ty Stanford <a1105183@Tys-MacBook-Air.local>
2017-09-05 00:46:24 +0930

    turn into package
    lots of changes

---
+++ R/fairclough.R
@@ -0,0 +1,38 @@
+#' Data from Fairclough (2017). Fitness, fatness and the reallocation of time between children’s daily movement behaviours: an analysis of compositional data
+#'
+#' A dataset containing z_bmi (outcome), time-use compositions (sl,sb,lpa,mvpa), and covariates
+#'  from the Fairclough (2017) paper. The data can be found in supp file 7 of the paper
+#'  at https://link.springer.com/article/10.1186/s12966-017-0521-z.
+#'
+#'  The variables in the data are as follows:
+#'
+#' \itemize{
+#'   \item  child_id
+#'   \item  school
+#'   \item  sex
+#'   \item  decimal_age
+#'   \item  imd_decile
+#'   \item  height mass
+#'   \item  bmi
+#'   \item  z_bmi
+#'   \item  itof_grade
+#'   \item  waist_circ
+#'   \item  whtr
+#'   \item  shuttles_20m
+#'   \item  wear_time
+#'   \item  sed
+#'   \item  lpa
+#'   \item  mpa
+#'   \item  vpa
+#'   \item  mvpa
+#'   \item  sleep
+#'   \item  min_in_day
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name fairclough
+#' @usage data(fairclough)
+#' @format A data frame with 169 rows and 21 variables
+#' @references Fairclough, Stuart J. and Dumuid, Dorothea and Taylor, Sarah and Curry, Whitney and McGrane, Bronagh and Stratton, Gareth and Maher, Carol and Olds, Timothy. Fitness, fatness and the reallocation of time between children’s daily movement behaviours: an analysis of compositional data. International Journal of Behavioral Nutrition and Physical Activity, 2017. 14(1): 64.
+NULL
---
+++ R/fat_data.R
@@ -0,0 +1,22 @@
+#' Randomly generated data to simulate child fat percentage regressed on time-use composiitonal data
+#'
+#' A dataset containing fat percentage (outcome), time-use compositions (sl,sb,lpa,mvpa), and covariates
+#'  (sibs,parents,ed). Note sl+sb+lpa+mvpa=1440minutes for each subject. The variables are as follows:
+#'
+#' \itemize{
+#'   \item fat. child fat percentage (11.29--29.99)
+#'   \item sl. daily sleep in minutes (283--765)
+#'   \item sb. sedentary behaviour in minutes (354--789)
+#'   \item lpa. low-intensity physical activity in minutes (157--507)
+#'   \item mvpa. moderate- to vigourous-intensity physical activity in minutes (35--155)
+#'   \item sibs. number of siblings (0,1,2,3,4)
+#'   \item parents. number of parents/caregivers at home (1,2)
+#'   \item ed. education level of parent(s) (0=high school, 1=diploma, 2=degree)
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name fat_data
+#' @usage data(fat_data)
+#' @format A data frame with 100 rows and 8 variables
+NULL
