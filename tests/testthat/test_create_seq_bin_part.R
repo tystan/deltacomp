f1be3c6334c400defd59da9420972a98ea485502
DrLundRasmussen <Chinehade@gmail.com>
2021-05-26 16:27:27 +0200

    Started on the vignette based on the package description already made.
    Having some issues with the use_vignette function, so don't think this is a proper vignette... probably just a markdown.

--- tests/testthat/test_create_seq_bin_part.R
+++ tests/testthat/test_create_seq_bin_part.R
@@ -1,8 +1,10 @@
 context("create_seq_bin_part() checks")
 
-test_that("create_seq_bin_part() states error if non-numerics or multiple values are used", {
+test_that("create_seq_bin_part() throws error if wrong inputs", {
   
   expect_error(create_seq_bin_part("b"))
   expect_error(create_seq_bin_part(c(2,3)))
   
 })
+
+
